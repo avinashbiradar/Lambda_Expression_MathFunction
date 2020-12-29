bad176c5cbf1cf063ef905f6609769a7b44a1de1
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-29 08:09:15 +0530

    Added:Develop Basic math functions using lambda expression

---
+++ src/main/java/IMathFunction.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+interface IMathFunction{
+    int calculate(int a,int b);
+
+}
---
+++ src/main/java/MathOperationApp.java
@@ -0,0 +1,21 @@
+
+public class MathOperationApp {
+
+    public static void main(String[] args) {
+        //use case 1
+        //way 1 ---IMathFunction add = ( int a, int b)->a+b;
+        IMathFunction add =Integer::sum;
+        //multiply
+        IMathFunction multiply = ( int a, int b)->a*b;
+        //divison
+        IMathFunction divison = ( int a, int b)->a/b;
+        //subtract
+        IMathFunction subtract = ( int a, int b)->a-b;
+
+        System.out.println("Addition "+add.calculate(6 ,3));
+        System.out.println("Multiply "+multiply.calculate(6 ,3));
+        System.out.println("Division "+divison.calculate(6 ,3));
+        System.out.println("Subtract "+subtract.calculate(6 ,3));
+
+    }
+}
