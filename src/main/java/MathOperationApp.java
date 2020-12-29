73a465f5c8f51c1855dc1c67b7177a1619a41a71
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-29 09:13:22 +0530

    Added:Passing lambda functions to show result of math operation

--- src/main/java/MathOperationApp.java
+++ src/main/java/MathOperationApp.java
@@ -17,5 +17,12 @@ public class MathOperationApp {
         System.out.println("Division "+divison.calculate(6 ,3));
         System.out.println("Subtract "+subtract.calculate(6 ,3));
 
+        //Passing Lambda Expression as Function Parameter yo Print Result using Static Function
+        IMathFunction.printResult(6,3,"Addition",add);
+        IMathFunction.printResult(6,3,"Subtract",subtract);
+        IMathFunction.printResult(6,3,"Multiply",multiply);
+        IMathFunction.printResult(6,3,"Division",divison);
+
+
     }
 }
